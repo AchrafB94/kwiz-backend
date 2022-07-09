a126854105f1ff25e83589cb34e1897acf209dd9
Achraf Bouanani <ashrafbouanani@gmail.com>
2022-07-09 17:59:06 +0100

    fixing port

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ app.use("/levels", Level);
 var Users = require("./routes/user.route");
 app.use("/users", Users);
 
+const PORT = 4000;
+
 app.listen(process.env.PORT || PORT, () => {
-  console.log("Server is running at port" + PORT);
+  console.log("Server is running at port " + PORT);
 });
